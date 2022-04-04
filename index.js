62326e7c3965a30bbb91d9f9d8f8cab66dbaaf44
Aris Vayas <aris.vayas@gmail.com>
2022-04-04 13:16:57 -0400

    commit tree 1cd17aae3892ae28a93b9dbea0bfadb4b8213cdb

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(a){
+  let feet = a
+  if (feet < 400){
+   return 'This one is on me!'
+  }
+  if (feet >= 400 && feet < 2000){
+    return 'That will be twenty bucks.'
+  }
+  if (feet >= 2000 && feet < 2500){
+    return 'I will gladly take your thirty bucks.'
+  }
+  if (feet >2500){
+    return 'No can do.'
+
+  }
+
+
+  
+
+}
+function ternaryCheckCity(city){
+  const isCity = city == "NYC" ? 'Ok, sounds good.': 'No go.';
+  return isCity
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  const happiness = tip;
+  let response;
+  switch(happiness){
+  case 'generous':
+    response =  'Thank you so much.'
+    break;
+    
+    
+      case 'not as generous':
+        response =  'Thank you.'
+        break;
+        
+   default:
+        response = "Bye."
+        break;
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+  }
+        return response
+  }
+
+
+
